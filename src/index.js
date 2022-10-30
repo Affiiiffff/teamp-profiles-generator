88830ac6c224cf350bdc4f3d3f53a1ccc989fc12
Afif Patel <afifpatel@hotmail.co.uk>
2022-10-30 20:09:08 +0000

    nee repo

---
+++ src/index.js
@@ -0,0 +1,154 @@
+const inquirer = require("inquirer");
+const Manager = require("../lib/Manger");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+// const generateHTML = require("./generateHTML");
+const fs = require("fs");
+const path = require("path");
+
+const managerInfo = async () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter your name",
+      },
+
+      {
+        type: "input",
+        name: "ID",
+        message: "Please enter your ID",
+      },
+      {
+        type: "input",
+        name: "Email",
+        message: "Please enter your Email",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter your Office number",
+      },
+    ])
+    .then((response) => {
+      const answers = response;
+      console.log(answers);
+    });
+};
+
+managerInfo();
+
+// const teamInfo = async () => {
+//   const answers = await inquirer.prompt([
+//     {
+//       type: "list",
+//       name: "role",
+//       message: "Please choose a role",
+//       choices: ["Engineer", "Intern"],
+//     },
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "Please enter your name",
+//     },
+//     {
+//       type: "input",
+//       name: "ID",
+//       message: "Please enter your ID",
+//     },
+//     {
+//       type: "input",
+//       name: "Email",
+//       message: "Please enter your Email",
+//     },
+//     {
+//       type: "input",
+//       name: "Github",
+//       message: "Please enter your Github",
+//     },
+//   ]);
+// };
+
+// const generateHTML = ({ name, ID, Email, officeNumber }) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>My Team</title>
+//     <link rel="stylesheet" href="./dist/assets/css/style.css" />
+//     <link
+//       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+//       rel="stylesheet"
+//       integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+//       crossorigin="anonymous"
+//     />
+//   </head>
+
+//   <section>
+//     <nav class="nav"><div>MY TEAM</div></nav>
+//   </section>
+
+//   <main class="main">
+//     <div class="card" style="width: 18rem">
+//       <div class="card-header">Manager: <span>${name}</span></div>
+//       <ul class="list-group list-group-flush">
+//         <li class="list-group-item">ID:<span>${ID}</span></li>
+//         <li class="list-group-item">Email:<span>${Email}</span></li>
+//         <li class="list-group-item">Office Number: <span>${officeNumber}</span></li>
+//       </ul>
+//     </div>
+
+//     <div class="card" style="width: 18rem">
+//       <div class="card-header">Engineer</div>
+//       <ul class="list-group list-group-flush">
+//         <li class="list-group-item">An item</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//       </ul>
+//     </div>
+
+//     <div class="card" style="width: 18rem">
+//       <div class="card-header">Engineer</div>
+//       <ul class="list-group list-group-flush">
+//         <li class="list-group-item">An item</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//       </ul>
+//     </div>
+//     <div class="card" style="width: 18rem">
+//       <div class="card-header">Engineer</div>
+//       <ul class="list-group list-group-flush">
+//         <li class="list-group-item">An item</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//       </ul>
+//     </div>
+
+//     <div class="card" style="width: 18rem">
+//       <div class="card-header">Intern</div>
+//       <ul class="list-group list-group-flush">
+//         <li class="list-group-item">An item</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//       </ul>
+//     </div>
+//   </main>
+
+//   <body>
+//     <script
+//       src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+//       integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+//       crossorigin="anonymous"
+//     ></script>
+//   </body>
+// </html>`
+
+// .then((answers) => {
+//   const htmlContent = generateMarkdown(answers);
+
+//   fs.writeFile("MyTeam.html",htmlContent , (err) =>
+//   err ? console.log(err) : console.log("Successfully created HTML")
+// );
