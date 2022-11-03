1783ddfb7352389504d6280eb63e54ab3d6ce243
Afif Patel <afifpatel@hotmail.co.uk>
2022-11-03 18:21:34 +0000

    hi

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,53 +1,38 @@
-const generateManager = function (manager) {
+const createManager = function (manager) {
   return `
-  <div class="col-4 mt-4">
-      <div class="card h-100">
-          <div class="card-header">
-              <h3>${manager.name}</h3>
-              <h4>Manager</h4>
-          </div>
-          <div class="card-body">
-              <p class="id">ID: ${manager.id}</p>
-              <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-              <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
-          </div>
-      </div>
-  </div>
+  <div class="card" style="width: 18rem">
+  <div class="card-header">Manager:<span>${manager.name}</span></div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID<span>${manager.id}</span></li>
+    <li class="list-group-item">Email<span>${manager.email}</span></li>
+    <li class="list-group-item">Office Number<span>${manager.officeNumber}</span></li>
+  </ul>
+</div>
   `;
 };
 
-const generateEngineer = function (engineer) {
+const createEngineer = function (engineer) {
   return `
-  <div class="col-4 mt-4">
-      <div class="card h-100">
-          <div class="card-header">
-              <h3>${engineer.name}</h3>
-              <h4>Engineer</h4>
-          </div>
-          <div class="card-body">
-              <p class="id">ID: ${engineer.id}</p>
-              <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-              <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
-          </div>
-      </div>
-  </div>
+  <div class="card" style="width: 18rem">
+  <div class="card-header">Manager:<span>${engineer.name}.name}</span></div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID<span>${engineer.id}</span></li>
+    <li class="list-group-item">Email<span>${manager.email}</span></li>
+    <li class="list-group-item">Office Number<span>${engineer.github}</span></li>
+  </ul>
+</div>
   `;
 };
 
-const generateIntern = function (intern) {
+const createIntern = function (intern) {
   return `
-  <div class="col-4 mt-4">
-      <div class="card h-100">
-          <div class="card-header">
-              <h3>${intern.name}</h3>
-              <h4>Intern</h4>
-          </div>
-          <div class="card-body">
-              <p class="id">ID: ${intern.id}</p>
-              <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-              <p class="school">School: ${intern.school}</p>
-          </div>
-  </div>
+  <div class="card" style="width: 18rem">
+  <div class="card-header">Manager:<span>${intern.name}</span></div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID<span>${intern.id}</span></li>
+    <li class="list-group-item">Email<span>${intern.email}</span></li>
+    <li class="list-group-item">Office Number<span>${intern.school}</span></li>
+  </ul>
 </div>
   `;
 };
